52be9b84798a6c8488618740656a93a225a10277
spyoungtech <spencer.young@spyoung.com>
2018-02-16 16:36:35 -0500

    apply solution

--- another_lib/steps.py
+++ another_lib/steps.py
@@ -1,8 +1,11 @@
 from behave import *
 import behave
+original_matcher = behave.matchers.current_matcher
 
 use_step_matcher('re')
 @given(u'I( do not)* use a step library')
 def step_impl(context, not_):
 	pass
 	
+
+behave.matchers.current_matcher = original_matcher
